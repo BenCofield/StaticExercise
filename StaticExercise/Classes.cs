9613acb85af16fb641f905e3d36809ba3c674f46
Ben Cofield <benrcofield@gmail.com>
2022-10-03 10:55:38 -0400

    first commit

---
+++ StaticExercise/Classes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise 
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius (double F)
+        {
+
+            double C = (5.0 / 9.0) * (F - 32);
+            return C;
+        }
+        public static double CelsiusToFahrenheit (double C)
+        {
+            double F = (9.0 / 5.0 * C) + 32;
+            return F;
+        }
+    }
+}
---
+++ StaticExercise/Program.cs
@@ -0,0 +1,11 @@
+﻿using StaticExercise;
+using System;
+
+
+double newYorkTempF = 52;
+double newYorkTempC = TempConverter.FahrenheitToCelsius(newYorkTempF);
+double londonTempC = 18;
+double londonTempF = TempConverter.CelsiusToFahrenheit(londonTempC);
+
+Console.WriteLine($"It is {newYorkTempF}°F ({Math.Round(newYorkTempC)}°C) in New York City today");
+Console.WriteLine($"It is {londonTempC}°C ({Math.Round(londonTempF)}°F) in London today");
